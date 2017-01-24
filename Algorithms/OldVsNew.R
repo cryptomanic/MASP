d106de9bc8cb2dc5ef7e3d201431112541e3bbb5
cryptomanic <deepakyadav.iitbhu@gmail.com>
2017-01-24 12:09:57 +0530

    add reproducible example to compare old and new algo

--- Algorithms/OldVsNew.R
+++ Algorithms/OldVsNew.R
@@ -39,3 +39,35 @@ setwd("blogdata/")
 # same item present more than once within a row
 data <- read.csv("blogData_train.csv", header = FALSE) 
 
+# Tue Jan 24 11:18:03 2017 ------------------------------
+# let's generate a data set(unbiased)
+
+generate.data <- function(nrow, ncol, rng) {
+  data <- matrix(nrow = nrow, ncol = ncol) 
+  for(i in 1:nrow) {
+    if(i%%2 == 0)
+      data[i, ] <- ncol:1
+    else
+      data[i, ] <- 1:ncol
+  }
+  old_data <- as.data.frame(data)
+  new_data <- data
+  new_data[1, 1] <- as.character(new_data[1, 1])
+  
+  list(old_data = old_data, new_data = new_data)
+}
+
+compareOandN <- function(nrow, ncol, rng, support, confidence) {
+  ldata <- generate.data(nrow, ncol, rng)
+  old_root <- generateMaspTree(data = ldata$old_data, support = support, confidence)
+  new_root <- generateMaspTree2(data = ldata$new_data, support = support, confidence)
+  list(old_root, new_root)
+}
+
+# reproducible comparison
+set.seed(10001)
+lroot <- compareOandN(10000, 100, 5, .001, .25)
+plot(lroot[[1]])
+plot(lroot[[2]])
+longestRuleSize(lroot[[1]])
+longestRuleSize(lroot[[2]])
